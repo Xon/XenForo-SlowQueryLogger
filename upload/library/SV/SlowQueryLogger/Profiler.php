cfb7e631a13f3754a24b6c3f50b78addad7a42d0
Xon <tazipper1@hotmail.com>
2017-02-12 15:44:45 +0800

    Log slow transactions

--- upload/library/SV/SlowQueryLogger/Profiler.php
+++ upload/library/SV/SlowQueryLogger/Profiler.php
@@ -2,67 +2,116 @@
 
 class SV_SlowQueryLogger_Profiler extends XFCP_SV_SlowQueryLogger_Profiler
 {
+    protected $slowQuery = 1.5;
+    protected $slowTransaction = 1;
     protected $reportSlowQueries = false;
+    protected $startTransactionTime = null;
+    protected $transactionEndQueryId = null;
+    protected $startedTransaction = 0;
 
     public function __construct($reportSlowQueries = true, $enabled = false)
     {
         $this->slowQuery = XenForo_Application::getOptions()->sv_slowquery_threshold;
+        $this->slowTransaction = XenForo_Application::getOptions()->sv_slowtransaction_threshold;
         $this->reportSlowQueries = $reportSlowQueries;
         $this->setEnabled($enabled);
     }
 
     public function queryStart($queryText, $queryType = null)
     {
-        if (!$this->_enabled && $this->reportSlowQueries)
+        if (!$this->reportSlowQueries)
         {
-            $this->_enabled = true;
-            try
+            return parent::queryStart($queryText, $queryType);
+        }
+        $captureQueryId = false;
+        switch ($queryText)
+        {
+            case 'begin':
+                // transaction start
+                if ($this->startedTransaction === 0)
+                {
+                    $this->startTransactionTime = microtime(true);
+                }
+                $this->startedTransaction += 1;
+                break;
+            case 'rollback':
+            case 'commit':
+                $this->startedTransaction -= 1;
+                if ($this->startedTransaction === 0)
+                {
+                    $captureQueryId = true;
+                }
+                break;
+        }
+
+        $old = $this->_enabled;
+        $this->_enabled = true;
+        try
+        {
+            $queryId = parent::queryStart($queryText, $queryType);
+        }
+        finally
+        {
+            $this->_enabled = $old;
+            if ($captureQueryId)
             {
-                return parent::queryStart($queryText, $queryType);
-            }
-            finally
-            {
-                $this->_enabled = false;
+                $this->transactionEndQueryId = $queryId;
             }
         }
-        return parent::queryStart($queryText, $queryType);
+        return $queryId;
     }
 
     public function queryEnd($queryId)
     {
-        if ($this->reportSlowQueries)
+        if (!$this->reportSlowQueries)
         {
-            $old = $this->_enabled;
-            $this->_enabled = true;
-            try
-            {
-                $ret = parent::queryEnd($queryId);
+            return parent::queryEnd($queryId);;
+        }
+        $old = $this->_enabled;
+        $this->_enabled = true;
+        $queryEndTime = null;
+        try
+        {
+            $ret = parent::queryEnd($queryId);
+            $queryEndTime = microtime(true);
 
-                if ($ret == self::STORED)
+            if ($ret == self::STORED)
+            {
+                $qp = $this->_queryProfiles[$queryId];
+                if ($qp->getElapsedSecs() >= $this->slowQuery)
                 {
-                    $qp = $this->_queryProfiles[$queryId];
-                    if ($qp->getElapsedSecs() >= $this->slowQuery)
+                    static $requestPaths = null;
+                    if ($requestPaths === null)
                     {
-                        static $requestPaths = null;
-                        if ($requestPaths === null)
-                        {
-                            $requestPaths = XenForo_Application::get('requestPaths');
-                        }
-                        XenForo_Error::logException(new Exception('Slow query detected: '.round($qp->getElapsedSecs(),4).' seconds, '.(empty($requestPaths['requestUri']) ? '': $requestPaths['requestUri'])), false);
-                        if (!$old)
-                        {
-                            unset($this->_queryProfiles[$queryId]);
-                            return self::IGNORED;
-                        }
+                        $requestPaths = XenForo_Application::get('requestPaths');
+                    }
+                    XenForo_Error::logException(new Exception('Slow query detected: '.round($qp->getElapsedSecs(),4).' seconds, '.(empty($requestPaths['requestUri']) ? '': $requestPaths['requestUri'])), false);
+                    if (!$old)
+                    {
+                        unset($this->_queryProfiles[$queryId]);
+                        $ret = self::IGNORED;
                     }
                 }
-                return $ret;
             }
-            finally
-            {
-                $this->_enabled = $old;
-            }
+
         }
-        return parent::queryEnd($queryId);
+        finally
+        {
+            $this->_enabled = $old;
+        }
+
+        if ($this->transactionEndQueryId !== null && $this->transactionEndQueryId === $queryId &&
+            ($queryEndTime - $this->startTransactionTime) >= $this->slowTransaction)
+        {
+            $this->transactionEndQueryId = null;
+            static $requestPaths = null;
+            if ($requestPaths === null)
+            {
+                $requestPaths = XenForo_Application::get('requestPaths');
+            }
+            XenForo_Error::logException(new Exception('Slow transaction detected: '.round($queryEndTime,4).' seconds, '.(empty($requestPaths['requestUri']) ? '': $requestPaths['requestUri'])), false);
+        }
+
+        return $ret;
     }
 }
